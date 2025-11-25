cac7f2ccf241a43673cd352962bc3ff618eb68db
Alina-Pishak <alinapishak@icloud.com>
2025-11-25 17:54:25 +0200

    fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,7 @@ interface Config {
   nodeEnv: string;
   mongoUri: string;
 }
+
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || 'development',
